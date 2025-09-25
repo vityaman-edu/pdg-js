d0409d869d77a31eea9a7611a737d0cc10df4759
vityaman <vityaman.dev@yandex.ru>
2025-09-25 13:07:11 +0300

    Print AST

--- src/pdg/playground.ts
+++ src/pdg/playground.ts
@@ -1,3 +1,24 @@
+import * as ts from 'typescript'
+
+function visit(node: ts.Node, indent = '', onNode: (indent: string, node: ts.Node) => void) {
+  onNode(indent, node)
+  ts.forEachChild(node, (child) => {
+    visit(child, indent + '  ', onNode)
+  })
+}
+
 export const play = (source: string) => {
-  return source + source
+  const file = ts.createSourceFile(
+    'source.ts',
+    source,
+    ts.ScriptTarget.ES2024,
+    /* setParentNodes */ true,
+    ts.ScriptKind.TS,
+  )
+
+  let output = 'AST:\n'
+  visit(file, /* indent: */ '', (indent, node) => {
+    output += indent + ts.SyntaxKind[node.kind] + '\n'
+  })
+  return output
 }
