e687ce46d640002191b7e98e07619187f7dab194
vityaman <vityaman.dev@yandex.ru>
2025-10-08 18:17:34 +0300

    Add loops example

--- src/ui/examples.ts
+++ src/ui/examples.ts
@@ -62,4 +62,22 @@ if (x % 4 == 1) {
 }
 `,
 
+  'Loop': `
+let x = 1
+while (x < 10) {
+  x += 1
+}
+
+do {
+  x -= 1
+} while (x > 0)
+
+for (;;) {
+  console.log('Yes')
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(\`i = \${i}\`)
+}
+`,
 }
